05ff8982ccc04bba2ac310b3f2ec3e6cfc0d75c8
WendaDeng <dwd1044898101@gmail.com>
2020-06-03 13:35:30 +0800

    solved largest-rectangle-in-histogram problem using C++

---
+++ Week_01/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int res = 0;
+        if (heights.size() == 0)    return res;
+
+        heights.push_back(0);   // sentinel，用于弹出处理最后一个元素
+        // 用一个 vector 模拟栈，保存遍历过的高度（按升序排列）
+        vector<int> index;
+        for (int i = 0; i < heights.size(); ++i) {
+            while (index.size() > 0 && heights[index.back()] > heights[i]) {
+                int h = heights[index.back()];
+                index.pop_back();
+                int leftbound = index.size() > 0 ? index.back() : -1;
+                res = max(res, (i - leftbound - 1) * h);
+            }
+            index.push_back(i);
+        }
+        return res;
+    }
+};
